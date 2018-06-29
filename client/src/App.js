f4e76cb2e3a058249abfe2ee8cc6e94da4e5af4c
unknown <jjernigan1065@gmail.com>
2018-06-29 18:36:36 -0400

    connected front end to back end with proxy. api working and pulling back articles from nyt. working on making dates optional.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Nav from './components/Nav/Nav';
+import Home from './components/home/home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+          <Nav/>
+        <Home />
       </div>
     );
   }
